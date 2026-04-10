b049637ca299d59d19454d19e0dbb90cfc14e726
do-haru <doharu05@naver.com>
2026-04-10 17:45:32 +0900

    feat: HouseCard 변경

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -7,10 +7,23 @@ const HouseCard = ({ item, onClick }) => {
       onClick={onClick}
       style={{ cursor: "pointer", border: "1px solid", marginBottom: "10px" }}
     >
-      <p>위치: {item.regionName}</p>
-      <p>면적: {item.area}m2</p>
-      <p>가격: {item.price}m2</p>
-      <p>이유: {item.reason}</p>
+      <p>id: {item.id}</p>
+      <p>house id: {item.house.id}</p>
+      <p>house 읍/면/동: {item.house.regionName}</p>
+      <p>house 리: {item.house.regionDetail}</p>
+      <p>house 면적: {item.house.area}</p>
+      <p>house 주택유형: {item.house.housetype}</p>
+      <p>house 빈집 상태 등급: {item.house.grade}</p>
+      <p>house 관리 기관명: {item.house.manager}</p>
+      <p>house 전화번호: {item.house.phone}</p>
+
+      <p>위도: {item.latitude}</p>
+      <p>경도: {item.longitude}m2</p>
+      <p>편의시설갯수: {item.facilityCount}m2</p>
+      <p>유동인구수준: {item.crowd}</p>
+      <p>연령대별 방문 비율 정보: {item.targetAgeRatio}</p>
+      <p>가격: {item.price}</p>
+      <p>점수: {item.score}</p>
     </div>
   );
 };
